3705a02bd76fe65046309ec6cf84f03cadd8f49c
Stina B <stinaboehmig@gmx.de>
2019-12-07 11:02:36 +0100

    create new branch

--- DTKartenspiel/Assets/Scripts/Camera.cs
+++ DTKartenspiel/Assets/Scripts/Camera.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-	GameObject[] cameraPoints = new GameObject[4];
-	public GameObject cameraPointConnector;
-	//array auff√ºllen
-	// Start is called before the first frame update
-	int players;
-	void Start()
-	{
-		int i;
-		for (i = 0; i < 4; i++)
-		{
-			cameraPoints[i] = cameraPointConnector.transform.GetChild(i).gameObject;
-		}
-		// player abh√§ngig vom networking/ multiplayer abh√§ngig
-		// player einer zahl 0-3
-		players = 3;
-		gameObject.transform.position = cameraPoints[players].transform.position;
-		gameObject.transform.rotation = cameraPoints[players].transform.rotation;
-	}
+    GameObject[] cameraPoints = new GameObject[4];
+    public GameObject cameraPointConnector;
+    //array auff¸llen
+    // Start is called before the first frame update
+    int players;
+    void Start()
+    {
+        int i;
+        for (i = 0; i < 4; i++)
+        {
+            cameraPoints[i] = cameraPointConnector.transform.GetChild(i).gameObject;
+        }
+        // player abh‰ngig vom networking/ multiplayer abh‰ngig
+        // player einer zahl 0-3
+        players = 3;
+        gameObject.transform.position = cameraPoints[players].transform.position;
+        gameObject.transform.rotation = cameraPoints[players].transform.rotation;
+    }
 }
---
+++ DTKartenspiel/Assets/Scripts/SingleGatterChoice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SingleGatterChoice : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public GameObject prefab;
+
+    [Header("Entrys")]
+    public bool a;
+    public bool b;
+    public bool c;
+
+    [Header("Exit")]
+    public bool y;
+
+    private Vector3 originPosition;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GetComponent<Image>().color = Color.black;
+        originPosition = transform.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GetComponent<Image>().color = Color.white;
+        Instantiate(prefab, Input.mousePosition, Quaternion.identity);
+        transform.position = originPosition;
+    }
+}
